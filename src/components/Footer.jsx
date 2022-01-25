00d10d9e8b7c70343171d538edddcd157ca109c5
Yasir <mdxyasir@gmail.com>
2022-01-25 20:41:46 +0000

    footer email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="footer" >
             <img src="./images/layered-waves.png" alt="footer-image" style={{width: "100%"}} />
             <a href="https://github.com/mdxyasir" target="_blank"><i className="fab fa-github"></i></a>
-            <a href="mailto:mdxyasir@gmail.com"><i className="fas fa-envelope"></i></a>
+            <a href="mailto:hello@mdxyasir.com"><i className="fas fa-envelope"></i></a>
             <a href="https://deviantart.com/mdxyasir" target="_blank"><i className="fab fa-deviantart"></i></a>
             <br/>
             <p1>Yasir・2022</p1>
