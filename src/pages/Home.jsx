85ff20e71b25ea3e720e4c76bba29525260bbdfc
Yasir <hello@mdxyasir.com>
2022-12-29 21:49:03 +0000

    💬 Added links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from '../styles/Home.module.css'
 
@@ -9,14 +10,27 @@ const Home = () => {
         <h1>Hello, I'm <span>Yasir</span>.</h1>
         <h2 className={styles.text}>Artist. Programmer. Student.</h2>
 
-        <a
-            href='mailto:hello@mdxyasir.com'
-            target='_blank'
-            rel='noreferrer'
-            className={styles.contact}
-        >
-            Contact Me
-        </a>
+        <Link to='portfolio' className={styles.contact}>View My Work</Link>
+
+        <div className={styles.links}>
+            <a 
+                href='https://github.com/mdxyasir'
+                target='_blank'
+                rel='noreferrer'
+            ><i className='fab fa-github'></i></a>
+
+            <a
+                href='mailto:hello@mdxyasir.com'
+                target='_blank'
+                rel='noreferrer'
+            ><i className='fas fa-envelope'></i></a>
+
+            <a
+                href='https://deviantart.com/mdxyasir'
+                target='_blank'
+                rel='noreferrer'
+            ><i className='fab fa-deviantart'></i></a>
+        </div>
 
     </div>
   )
